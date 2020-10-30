e592a3c023c94eed595639879e078ce1d6a1cbee
Thiago Yuri <thiago.souza@ccc.ufcg.edu.br>
2020-10-30 09:17:35 -0300

    Update and rename hello.cpp to hello-thiagoyeds.cpp

---
+++ c++/hello-thiagoyeds.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Hacktoberfest 2020! My name is thiagoyeds!";
+    return 0;
+}
--- c++/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello Hacktoberfest 2020!";
-    return 0;
-}
