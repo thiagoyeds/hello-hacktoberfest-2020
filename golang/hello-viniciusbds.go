c1923034f33144a0f166cdc4bd280e4e690c27a5
Vinicius Barbosa <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2020-10-31 20:40:33 -0300

    Update hello-viniciusbds.go

--- golang/hello-viniciusbds.go
+++ golang/hello-viniciusbds.go
@@ -5,6 +5,5 @@ import(
 )
 
 func main() {
-    name := "Vinicius"
-    fmt.Println("Hello Hacktoberfest 2020! My name is", name)
+    fmt.Println("Hello Hacktoberfest 2020! My name is viniciusbds!")
 }
