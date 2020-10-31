6903b1fb6af2b759f713c547cf54442403b3d5d3
Diamond Purvis <Diamond.JPurvis@ibm.com>
2020-10-31 15:27:27 -0500

    hello in python

---
+++ python/hello-DiamondPurvis.py
@@ -0,0 +1,19 @@
+#Hard Coded
+hello = ("Hello Hacktoberfest 2020! ")
+
+name = ("My name is DiamondPurvis!")
+
+greeting = hello + name
+
+print(greeting)
+
+#Coded with input
+
+hello = input("What year is it? ",)
+#enter 2020!
+name = input("What is your name? ")
+#enter diamond!
+
+greeting1 =  hello 
+greeting2 = name
+print("Hello Hacktoberfest", greeting1,"My name is", name)
